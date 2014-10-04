3fd65214e8bd569bb48178abf485dfc8647daee5
Pepijn Kenter <titusjan@gmail.com>
2014-10-04 14:09:18 +0200

    Removed unused modules.

--- libpepeye/statstablemodel.py
+++ libpepeye/statstablemodel.py
@@ -5,9 +5,9 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-import logging, sys, pstats
+import logging, pstats
 
-from .version import PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_URL, DEBUGGING, USE_PYQT
+from .version import USE_PYQT
 from .utils import check_class
 
 if USE_PYQT:
